e54c66f362775223d671cfe402a803bd05699799
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-08-03 22:46:05 -0300

    agreegando notificaciones

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const container = document.querySelector(".container")
+const addTaskDiv = document.querySelector(".add-task")
 const scrollDiv = document.querySelector(".scroll")
 const taskInput = document.getElementById("taskInput")
 const taskButton = document.querySelector(".taskButton")
@@ -6,6 +7,20 @@ const taskButton = document.querySelector(".taskButton")
 taskButton.addEventListener("click", addTask)
 
 function addTask() {
+
+  if (taskInput.value.trim() === "") {
+    const error = document.createElement("p")
+    error.textContent = "El campo de tarea no puede ir vacío"
+    error.classList.add("error")
+    addTaskDiv.append(error)
+    return
+  }
+
+  // Eliminar mensaje de error existente si existe
+  const existingError = container.querySelector('.error');
+  if (existingError) {
+    existingError.remove();
+  }
 
   const taskDiv = document.createElement("div")
   taskDiv.classList.add("task")
@@ -36,10 +51,26 @@ function addTask() {
 function deleteTask(event) {
   // me devuelve div más cercano con la clase tarea
   const taskDiv = event.target.closest(".task")
-  taskDiv.remove()  
+  const deleteNotification = document.createElement("p")
+  deleteNotification.textContent = "Tarea Eliminada"
+  deleteNotification.classList.add("delete-notification")
+  document.querySelector("body").append(deleteNotification)
+  setTimeout(() => {
+    const notification = document.querySelector(".delete-notification")
+    notification.remove()
+  }, 1000);
+  taskDiv.remove()
 }
 
 function markAsComplete(event) {
   const taskDiv = event.target.closest(".task")
+  const completeNotification = document.createElement("p")
+  completeNotification.textContent = "Tarea Completada"
+  completeNotification.classList.add("complete-notification")
+  document.querySelector("body").append(completeNotification)
+  setTimeout(() => {
+    const notification = document.querySelector(".complete-notification")
+    notification.remove()
+  }, 1000);
   taskDiv.remove()
 }
